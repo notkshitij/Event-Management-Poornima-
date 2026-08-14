66b1fbe85466b8472cc1032872eb5a01be0fb69a
Kshitij Jain <2024btechaimlkshitij18489@poornima.edu.in>
2026-08-15 02:07:29 +0530

    refactor: clean up unused pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SplashScreen from './pages/SplashScreen.jsx'
 import Explore from './pages/Explore.jsx'
-import ProblemStatements from './pages/ProblemStatements.jsx'
-import InfoPage from './pages/InfoPage.jsx'
 import NotFound from './pages/NotFound.jsx'
 
 function App() {
@@ -12,8 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashScreen />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/problems" element={<ProblemStatements />} />
-        <Route path="/info/:type" element={<InfoPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
