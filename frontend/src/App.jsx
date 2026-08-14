06c061144632ea05291495d01888d2d26c6c817a
DarkPegasus24 <2024btechaimlmanvendra19170@poornima.edu.in>
2026-08-15 00:17:35 +0530

    chore: register new routes in App router and update package locks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SplashScreen from './pages/SplashScreen.jsx'
 import Explore from './pages/Explore.jsx'
+import ProblemStatements from './pages/ProblemStatements.jsx'
+import InfoPage from './pages/InfoPage.jsx'
 import NotFound from './pages/NotFound.jsx'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashScreen />} />
         <Route path="/explore" element={<Explore />} />
+        <Route path="/problems" element={<ProblemStatements />} />
+        <Route path="/info/:type" element={<InfoPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
